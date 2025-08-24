88089463fd17c1796c5e17b9a456ca93f7656ab4
Andre Tobois <tobois.andre@gmail.com>
2025-08-24 16:19:33 -0400

    Added more content for gear page.

--- src/app/gear/page.tsx
+++ src/app/gear/page.tsx
@@ -4,46 +4,59 @@ import Image from "next/image";
 const dummyGear = [
   {
     name: "Sena OutMotorcycle Helmet",
-    image: "/gear/helmet.jpg",
-    link: "#",
+    image: "/senaHelmet.jpg",
+    link: "https://www.amazon.com/Sena-Outrush-Bluetooth-Motorcycle-Intercom/dp/B09LRJ4SLZ/ref=sr_1_1_sspa?crid=2ZH4PQYYZX8KV&dib=eyJ2IjoiMSJ9.kF6SRGrsRDbN6-gmdCr4gpifbKufvMWMCMqvPpxScMLJq9Yh9y2BuEce5t6UVBkTOe94uiordZUX4up_plOaJUx_9kiDrfl41tt5eAy30BZioKViNy9Itgp5zxR41bAWaIFb23j40jwd5nQmz4HFS9aqbTSxOnX9k4KrLwnwC6rXqgOtqXA81n2wVBTOYw_7yW-ZK1g7m5Ziy8eyweUzTAypzkG_guNGLP6yb_LBiIhILTcdaLFuMSxKuqTc8_wsv9JEdsBFHrs_-Alslo0mD0QU23uUcEj2VLs6mN_mD-s.bFYBAdvw3Kfap_6551C-WEkd_7P2x4H1LweEErshwQ8&dib_tag=se&keywords=sena%2Boutrush%2Br%2Bbluetooth%2Bhelmet&qid=1756064617&sprefix=sena%2Boutrush%2Br%2Bb%2Caps%2C153&sr=8-1-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9hdGY&th=1",
     description: "Ride with confidence. DOT-certified full-face helmet.",
   },
   {
-    name: "Riding Gloves",
-    image: "/gear/gloves.jpg",
-    link: "#",
-    description: "Leather tactical gloves for maximum grip and protection.",
+    name: "INBIKE Breathable Mesh Motorcycle",
+    image: "/gloves.jpg",
+    link: "https://www.amazon.com/dp/B0861VWQKJ?ref=ppx_yo2ov_dt_b_fed_asin_title&th=1",
+    description: "Mesh tactical gloves for maximum grip and protection.",
   },
   {
     name: "DJi Osmo 4",
-    image: "/gear/dji4.jpg",
-    link: "#",
+    image: "/djiOsmo4.jpg",
+    link: "https://www.amazon.com/dp/B0C783YNW7?ref=ppx_yo2ov_dt_b_fed_asin_title&th=1",
     description: "Capture every turn in crisp 4K — motovlogger's choice.",
   },
   {
     name: "DJi Osmo Chin Mount",
-    image: "/gear/gopro12.jpg",
+    image: "/osmoHelmetMount.jpg",
     link: "#",
     description:
       "The standard for action cam footage. Sharp, stable, and waterproof.",
   },
   {
-    name: "Rokform Motorcycle Phone Mount",
-    image: "/gear/rokform.jpg",
-    link: "#",
-    description: "Rock-solid grip for your phone. Never drop mid-ride.",
-  },
-  {
     name: "CamelBak HydroBak",
-    image: "/gear/cardo.jpg",
-    link: "#",
+    image: "/camelBak.jpg",
+    link: "https://www.amazon.com/dp/B01L8JENFO/ref=twister_B0F16N4MHP?_encoding=UTF8&th=1",
     description: "Crystal-clear water when you need it for the ride.",
   },
   {
     name: "AlpineStars Armored TSP V2 mesh Jacket ",
-    image: "/gear/jacket.jpg",
-    link: "#",
+    image: "/alpineJacket.jpg",
+    link: "https://www.amazon.com/dp/B0DXZMRBN6?ref=ppx_yo2ov_dt_b_fed_asin_title&th=1&psc=1",
     description: "Urban stealth style meets high-impact protection.",
+  },
+  {
+    name: "JOYROOM Motorcycle Phone Mount",
+    image: "/phoneMount.jpg",
+    link: "https://www.amazon.com/dp/B09PVFN7LG?ref=ppx_yo2ov_dt_b_fed_asin_title&th=1",
+    description: "Sturdy phone holder for motorcycle riders.",
+  },
+  {
+    name: "PSLER Engine Guard Highway Crash Bars for CMX Rebel 1100 ",
+    image: "/crashBarRebel.jpg",
+    link: "https://www.amazon.com/dp/B0CD82Q5H7?ref=ppx_yo2ov_dt_b_fed_asin_title",
+    description: "Crash guards for Honda Rebel CMX Rebel 1100.",
+  },
+  {
+    name: "BANGTING 4Ft Bike Chain Lock Heavy Duty with 110dB Alarm",
+    image: "/chainLock.jpg",
+    link: "https://www.amazon.com/dp/B0DTJ85QJH?ref=ppx_yo2ov_dt_b_fed_asin_title&th=1",
+    description:
+      "Bicycle Lock Heavy Duty Anti Theft Security Bike Locks with Key for Motorcycles (Black)",
   },
 ];
 
